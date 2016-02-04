57894053f35fe3ef842dde5a9b8066f36c3cdb0d
ebadrehman5190 <ebadrehman5190@gmail.com>
2016-02-04 17:05:47 +0500

    connected git hub

---
+++ new 2.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+?>
+
+</body>
+</html>
