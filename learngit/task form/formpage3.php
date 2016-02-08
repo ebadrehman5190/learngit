a7877a0b1129c945ff57b66fef4bdf9685695027
ebadrehman5190 <ebadrehman5190@gmail.com>
2016-02-08 14:10:29 +0500

    form 2nd step

---
+++ learngit/task form/formpage3.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<body>
+
+<form action="formpage4.php" method="post">
+FirstName:<input type="text" name="fname"><br>
+LastName:<input type="text" name="lname"><br>
+<input type="submit" value="submit">
+</form>
+
+</body>
+</html>
---
+++ learngit/task form/formpage4.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<body>
+
+<?php
+echo $_POST["fname"];
+echo "<br>";
+echo $_POST["lname"];
+?>
+
+</body>
+</html>
